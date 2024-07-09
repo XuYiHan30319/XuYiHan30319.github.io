f154fbbbdba2cd225a01007ee963b6aec86ec550
许一涵 <81723334@qq.com>
2024-07-09 22:23:09 +0800

    测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://XuYiHan30319.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
